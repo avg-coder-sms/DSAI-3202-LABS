645ac48ee6ab69def1ba3546f2406d64cd669618
60301758 <60301758@udst.edu.qa>
2025-03-06 23:18:16 +0300

    Final modifications

--- python_project_lab/src/.ipynb_checkpoints/sequential-checkpoint.py
+++ python_project_lab/src/.ipynb_checkpoints/sequential-checkpoint.py
@@ -1,16 +1,13 @@
 import time
-from functions import sequential_sum
+from src.functions import sequential_sum
 
-# Input for large number
-n = 10000000
+def run_sequential(n):
+    # Measure execution time
+    start_time = time.time()
+    sum_result = sequential_sum(n)
+    end_time = time.time()
 
-# Measure execution time
-start_time = time.time()
-sum_result = sequential_sum(n)
-end_time = time.time()
+    execution_time = end_time - start_time
 
-execution_time = end_time - start_time
-
-print(f"Sequential Sum: {sum_result}")
-print(f"Execution Time: {execution_time} seconds")
-
+    print(f"Sequential Sum: {sum_result}")
+    print(f"Execution Time: {execution_time} seconds")
--- python_project_lab/src/parallel_processes.py
+++ python_project_lab/src/parallel_processes.py
@@ -1,13 +1,13 @@
 import time
-from functions import parallel_sum_multiprocessing
+from src.functions import parallel_sum_multiprocessing
 
-# Measure execution time for multiprocessing approach
-num_processes = 4
-start_time = time.time()
-sum_result_multiprocessing = parallel_sum_multiprocessing(n, num_processes)
-end_time = time.time()
+def run_processes(n, num_processes):
+    # Measure execution time for multiprocessing approach
+    start_time = time.time()
+    sum_result_multiprocessing = parallel_sum_multiprocessing(n, num_processes)
+    end_time = time.time()
 
-execution_time_multiprocessing = end_time - start_time
+    execution_time_multiprocessing = end_time - start_time
 
-print(f"Multiprocessing Sum: {sum_result_multiprocessing}")
-print(f"Execution Time (Multiprocessing): {execution_time_multiprocessing} seconds")
+    print(f"Multiprocessing Sum: {sum_result_multiprocessing}")
+    print(f"Execution Time (Multiprocessing): {execution_time_multiprocessing} seconds")
